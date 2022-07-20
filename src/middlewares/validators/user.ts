73be76669b81f5e7cb2356c497efc0722e3f55bb
Youngwoo Kim <yw.sean.kim@gmail.com>
2022-07-20 22:08:31 +0900

    feat: implement username policy check

--- src/middlewares/validators/user.ts
+++ src/middlewares/validators/user.ts
@@ -1,6 +1,7 @@
 import { body, CustomValidator } from 'express-validator';
 import User, { availableThemes } from '../../models/User';
 import { inMySQLIntRange, toBoolean, toDate } from './utils';
+import isURL from 'validator/lib/isURL';
 
 const isUserNameAvailable: CustomValidator = async (value: string, { req }) => {
   if (req.user) {
@@ -11,24 +12,77 @@ const isUserNameAvailable: CustomValidator = async (value: string, { req }) => {
   return user;
 };
 
+export const followsUserNamePolicy: CustomValidator = (value) => {
+  // Check length
+  if (value.length < 3) throw new Error('ERR_USERNAME_TOO_SHORT');
+  if (value.length > 32) throw new Error('ERR_USERNAME_TOO_LONG');
+
+  // ignore cases
+  value = value.toLowerCase();
+
+  // Only allow alphanumeric characters, underscores, dashes, and periods
+  const allowed = /[^a-z|A-Z|0-9|\-|_|.]/;
+  if (allowed.test(value)) throw new Error('ERR_USERNAME_ILLEGAL_CHARS');
+
+  // Special characters cannot appear more than 2 times in a row
+  const consecutiveSpecials = /[-|_|.]{2,}/;
+  if (consecutiveSpecials.test(value))
+    throw new Error('ERR_USERNAME_CONSECUTIVE_SPECIAL_CHARS');
+
+  // Cannot start nor end with a special character
+  const startsOrEndsWithSpecials = /^[-|_|.]|[-|_|.]$/;
+  if (startsOrEndsWithSpecials.test(value))
+    throw new Error('ERR_USERNAME_START_OR_END_WITH_SPECIAL_CHARS');
+
+  // Cannot contain certain words
+  const illegalWords = ['admin', 'sigmate', 'facade'];
+  const valueWithoutSpecials = value.replace(/[-|_|.]/g, '');
+  const containIllegalWords = illegalWords
+    .map((iw) => valueWithoutSpecials.includes(iw))
+    .reduce((p, c) => {
+      return p || c;
+    }, false);
+  if (containIllegalWords) throw new Error('ERR_USERNAME_ILLEGAL_WORDS');
+
+  // Cannot be a URL
+  if (isURL(value)) throw new Error('ERR_USERNAME_IS_URL');
+
+  // Same character cannot appear more than 4 times in a row
+  const consecutiveChars = /(\w)\1\1{3,}/;
+  if (consecutiveChars.test(value))
+    throw new Error('ERR_USERNAME_CONSECUTIVE_CHARS_4');
+
+  // Some characters cannot appear more than 3 times in a row
+  const consecutiveIOs = /[iI1]{4,}|[oO0]{4,}/;
+  if (consecutiveIOs.test(value))
+    throw new Error('ERR_USERNAME_CONSECUTIVE_CHARS_3');
+
+  return true;
+};
+
+export const validateUserName = body('userName')
+  .optional()
+  .escape()
+  .trim()
+  .stripLow()
+  .isString()
+  .withMessage('NOT_STRING')
+  .bail()
+  .isLength({ min: 3 })
+  .withMessage('TOO_SHORT')
+  .bail()
+  .isLength({ max: 32 })
+  .withMessage('TOO_LONG')
+  .bail()
+  .custom(isUserNameAvailable)
+  .withMessage('DUPLICATE')
+  .bail()
+  .custom(followsUserNamePolicy);
+
 export const validateUserPatch = [
   body('userId').optional().isEmpty().withMessage('UNKNOWN'),
   body('googleAccountId').optional().isEmpty().withMessage('UNKNOWN'),
-  body('userName')
-    .optional()
-    .escape()
-    .trim()
-    .stripLow()
-    .isString()
-    .withMessage('NOT_STRING')
-    .bail()
-    .isLength({ min: 1 })
-    .withMessage('TOO_SHORT')
-    .bail()
-    .isLength({ max: 32 })
-    .withMessage('TOO_LONG')
-    .custom(isUserNameAvailable)
-    .withMessage('DUPLICATE'),
+  validateUserName,
   body('email')
     .optional()
     .escape()
