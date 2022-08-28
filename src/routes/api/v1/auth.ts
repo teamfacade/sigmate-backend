1dd944266a5535355b4b658b93875ff9dec2d2b2
Youngwoo Kim <yw.sean.kim@gmail.com>
2022-08-28 15:41:58 +0900

    rename handlebadrequest

--- src/routes/api/v1/auth.ts
+++ src/routes/api/v1/auth.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import handleBadRequest from '../../../middlewares/handleBadRequest';
+import { validateGoogleAuthCode } from '../../../middlewares/validators/auth';
 import { handleGoogleOauth } from '../../../services/controllers/oauth/google';
 
 const authRouter = express.Router();
 
-authRouter.post('/google', handleGoogleOauth);
+authRouter.post(
+  '/google',
+  validateGoogleAuthCode,
+  handleBadRequest,
+  handleGoogleOauth
+);
 
 export default authRouter;
