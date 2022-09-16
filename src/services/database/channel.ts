7cf65299ba4f61065c87d4c3f6218e9c5e9a033c
heitzes <heitz@yonsei.ac.kr>
2022-09-16 18:42:02 +0900

    typo

--- src/services/database/channel.ts
+++ src/services/database/channel.ts
@@ -4,7 +4,7 @@ import SequelizeError from '../../utils/errors/SequelizeError';
 export const getAllChannels = async () => {
   try {
     return await Channel.findAll({
-      attributes: ['discord_channel', 'twitter_channel', 'collection'],
+      attributes: ['discord_channel', 'twitter_channel', 'collection_id'],
     });
   } catch (error) {
     throw new SequelizeError(error as Error);
