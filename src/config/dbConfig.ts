6b25906157885c1eb1ff1314c77f00dfa9938213
Youngwoo Kim <yw.sean.kim@gmail.com>
2022-09-28 19:40:15 +0900

    chore/env vars cleanup

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,17 +1,24 @@
 const dbConfig = {
   development: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_DEV,
     port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_DATABASE_DEV,
+  },
+  test: {
+    host: process.env.DB_HOST_TEST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASE_TEST,
   },
   production: {
-    host: process.env.DB_AWS_HOST,
+    host: process.env.DB_HOST_PROD,
     port: process.env.DB_PORT,
-    user: process.env.DB_AWS_USERNAME,
-    password: process.env.DB_AWS_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
   },
 };
 
