6ad69c1e062e6dfef08dc5190c388457b4b86614
heitzes <heitz@yonsei.ac.kr>
2022-10-12 18:44:45 +0900

    test to image

---
+++ src/routes/api/v1/image.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { passportJwtAuth } from '../../../middlewares/authMiddlewares';
+import handleBadRequest from '../../../middlewares/handleBadRequest';
+import imageUploader from '../../../middlewares/imageUploader';
+import { validateCreateImage } from '../../../middlewares/validators/image';
+import { createImageController } from '../../../services/image';
+
+const imageRouter = express.Router();
+
+imageRouter.post(
+  '/upload',
+  validateCreateImage,
+  handleBadRequest,
+  imageUploader.single('image'),
+  passportJwtAuth,
+  createImageController
+);
+
+export default imageRouter;
--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -6,7 +6,7 @@ import forumRouter from './forum';
 import profileRouter from './profile';
 import userRouter from './user';
 import wikiRouter from './wiki';
-import testRouter from './test';
+import imageRouter from './image';
 
 const v1Router = express.Router();
 
@@ -14,7 +14,7 @@ v1Router.use('/user', userRouter);
 v1Router.use('/auth', authRouter);
 v1Router.use('/profile', profileRouter);
 v1Router.use('/forum', forumRouter);
-v1Router.use('/s3', testRouter);
+v1Router.use('/image', imageRouter);
 v1Router.use('/wiki', wikiRouter);
 v1Router.use('/calendar', calendarRouter);
 v1Router.use('/wh', announcementRouter);
