f57f89e3b2ada189b287c47d0abd46ff389ccef7
Youngwoo Kim <yw.sean.kim@gmail.com>
2022-11-29 15:53:44 +0900

    fix/do not sync in production

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ checkEnv();
 import express from 'express';
 import setupExpress from './src/loaders/setupExpress';
 import setupRoutes from './src/loaders/setupRoutes';
-import syncDatabase from './src/loaders/syncDatabase';
+// import syncDatabase from './src/loaders/syncDatabase';
 import setupErrorHandlers from './src/loaders/setupErrorHandlers';
 
 const app = express();
 setupExpress(app);
 setupRoutes(app);
 setupErrorHandlers(app);
-syncDatabase(); // TODO remove in production
+// syncDatabase(); // TODO remove in production
 
 app.listen(app.get('port'), () => {
   console.log(`✅ Server listening on port ${app.get('port')}...`);
